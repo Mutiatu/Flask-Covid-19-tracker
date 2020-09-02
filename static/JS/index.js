9ba49e984f2cd09cd77423f11c681328cf7f00ee
Mutiatu <68806693+Mutiatu@users.noreply.github.com>
2020-09-02 08:26:57 -0700

    Add files via upload

---
+++ static/JS/index.js
@@ -0,0 +1,20 @@
+let countriesElem = document.getElementById("countries"),
+TotalElem = document.getElementById("Total"),
+ActivelElem = document.getElementById("Active"),
+RecoverdElem = document.getElementById("Recovered"),
+DeathElem = document.getElementById("Death"),
+allCountriesElem = document.getElementById("all-countries");
+
+function fectCovidData(country) {
+
+//get data from api
+}
+
+function populateTableData() {}
+
+function displayCountryData(country) {}
+
+function populateCountryList() {}
+
+TotalElem.innerText = "Total Change";
+RecoverdElem.innerText = "1b";
